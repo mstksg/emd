8e6120f4f2cf24cd1fdb5b5fc548945421c74825
Justin Le <justin@jle.im>
2019-09-01 00:14:03 -0700

    cleaner benchmarks

--- bench/bench.hs
+++ bench/bench.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables                      #-}
 {-# LANGUAGE TypeApplications                         #-}
 {-# LANGUAGE TypeInType                               #-}
 {-# LANGUAGE TypeOperators                            #-}
@@ -11,6 +12,7 @@ import           Data.Complex
 import           Data.Maybe
 import           GHC.TypeNats
 import           Numeric.EMD
+import           Text.Printf
 import qualified Data.Vector       as UV
 import qualified Data.Vector.Sized as V
 import qualified Numeric.FFT       as FFT
@@ -33,12 +35,17 @@ main = do
     itest4096  <- evaluate . force $ emd defaultEO test4096
     itest16384 <- evaluate . force $ emd defaultEO test16384
 
+    let imfs256   = length . emdIMFs $ itest256
+        imfs1024  = length . emdIMFs $ itest1024
+        imfs4096  = length . emdIMFs $ itest4096
+        imfs16384 = length . emdIMFs $ itest16384
+
     defaultMainWith defaultConfig [
         bgroup "emd"
-          [ bench "256"   $ nf (emd defaultEO) test256
-          , bench "1024"  $ nf (emd defaultEO) test1024
-          , bench "4096"  $ nf (emd defaultEO) test4096
-          , bench "16384" $ nf (emd defaultEO) test16384
+          [ bench (printf "256 (%d imfs)"   imfs256  ) $ nf (emd defaultEO) test256
+          , bench (printf "1024 (%d imfs)"  imfs1024 ) $ nf (emd defaultEO) test1024
+          , bench (printf "4096 (%d imfs)"  imfs4096 ) $ nf (emd defaultEO) test4096
+          , bench (printf "16384 (%d imfs)" imfs16384) $ nf (emd defaultEO) test16384
           ]
       , bgroup "iemd"
           [ bench "256"   $ nf iemd itest256
@@ -53,9 +60,11 @@ generateData
     => MWC.GenIO
     -> IO (V.Vector (2^n) Double)
 generateData g = fmap (fmap realPart . ifftSized) . V.generateM $ \i ->
-    let i' = sqrt . recip . fromIntegral $ i
+    let i' = recip . (+ 1) . fromIntegral $ i
     in  (:+) <$> MWC.uniformR (-i', i') g
              <*> MWC.uniformR (-i', i') g
+  where
+
 
 ifftSized
     :: KnownNat n
